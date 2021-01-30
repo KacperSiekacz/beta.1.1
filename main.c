0d49d998f750da3e75b00eca340f09f8075cbec1
kacper.s <kahty11@gmail.com>
2021-01-30 09:37:11 +0100

    1.02

--- main.c
+++ main.c
@@ -3,5 +3,11 @@
 int main() {
     printf("Hello, World!\n");
 
+    printf("dodajmy siebie\n");
+    printf("jestem\n");
+    double potega;
+    potega=0.1;
+    for(int i=0; i<=19; i++)
+        printf("10 do potegi %*d= %*.f \n",2,i, 20, potega=potega*10);
     return 0;
 }
